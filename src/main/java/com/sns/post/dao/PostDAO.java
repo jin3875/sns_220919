1f6f89a5ac61d8c0d4bf1d8e3ac8b4e2e3ff6766
test <test@test.com>
2023-01-12 18:11:06 +0900

    초기 환경 세팅

---
+++ src/main/java/com/sns/post/dao/PostDAO.java
@@ -0,0 +1,13 @@
+package com.sns.post.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostDAO {
+	
+	public List<Map<String, Object>> selectPostListTEST();
+
+}
